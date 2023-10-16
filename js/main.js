a71983f74b7f071c95d1d47d90551436b6fbac9e
ehitajate-ivanov <137901830+ehitajate-ivanov@users.noreply.github.com>
2023-10-16 18:56:42 +0300

    сщььше

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(".benefits-slider").slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+        autoplay: true,
+    })
+});
